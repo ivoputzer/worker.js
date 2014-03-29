e7aef3c41ca4a5d67a7768a0edb96421244992ba
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2014-03-29 22:40:50 +0100

    implementing other console methods

--- src.js
+++ src.js
@@ -1,14 +1,24 @@
-/*
-
-  THIS SECITON ACTS LIKE A WORKER
-
-*/ if (typeof WorkerGlobalScope !== 'undefined' && this instanceof WorkerGlobalScope) {
-
+// this section runs within a worker later on
+if (typeof WorkerGlobalScope !== 'undefined' && this instanceof WorkerGlobalScope) {
   onmessage = function(e) {
     self.console = {
-      _signal: e.ports[0],
       log: function () {
-        console._signal.postMessage(['log', Array.prototype.slice.call(arguments)])
+        e.ports[0].postMessage(['log', Array.prototype.slice.call(arguments)])
+      },
+      warn: function () {
+        e.ports[0].postMessage(['warn', Array.prototype.slice.call(arguments)])
+      },
+      error: function () {
+        e.ports[0].postMessage(['error', Array.prototype.slice.call(arguments)])
+      },
+      info: function () {
+        e.ports[0].postMessage(['info', Array.prototype.slice.call(arguments)])
+      },
+      count: function () {
+        e.ports[0].postMessage(['count', Array.prototype.slice.call(arguments)])
+      },
+      debug: function (){
+        e.ports[0].postMessage(['debug', Array.prototype.slice.call(arguments)])
       }
     }
     onmessage = null
@@ -16,31 +26,20 @@
   }
 }
 
-/*
-
-  THIS SECTION RUNS ON THE MAIN THREAD
-
-*/ else {
-
+// this section runs on the main thread
+else {
   var script = document.scripts[document.scripts.length -1]
 
   window.NativeWorker = Worker
-
   Object.defineProperty(window, 'Worker', {writable: true})
 
   window.Worker = function Worker (url) {
-
     var signal = new MessageChannel
       , worker = new NativeWorker(script.src + '#' + url)
-
     worker.postMessage(url, [signal.port2])
-
     signal.port1.onmessage = function (e) {
       console[e.data[0]].apply(console, e.data[1])
     }
-
     return worker
-
   }
-
 }
